fc97f14416352250b4a97227ce7075922355ece8
deepaksharma124 <100397249+deepaksharma124@users.noreply.github.com>
2022-02-25 09:31:32 +0530

    Create Kubearmor-install.sh

---
+++ Kubearmor-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kubectl get pods -n kube-system | grep kubearmor
+
+if  [  $? -eq "0" ]
+then
+ echo "Kubearmor exist here."
+elif
+ karmor version
+then 
+ karmor install
+elif
+ curl -sfL https://raw.githubusercontent.com/kubearmor/kubearmor-client/main/install.sh | sudo sh -s -- -b /usr/local/bin
+then 
+ karmor install
+fi
+echo "Congratulation! Kubearmor is Ready here."
+
